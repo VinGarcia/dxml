fdfd077cb736c7458ecc70d25e39fa27674a1d63
Vinícius Garcia <vingarcia00@gmail.com>
2016-06-10 16:20:23 -0300

    Make `toXml` an iteractive function

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+# This project was forked from dict2xml
 """
   Simple xml serializer.
 
@@ -42,53 +43,108 @@
     </children>
     </family>
 """
+
+from sys import version_info
+py2 = version_info[0] == 2
+
+charmap = [
+  # Note that `&->&amp;` must be the first item.
+  ('&', '&amp'),
+  ('<', '&lt;'),
+  ('>', '&gt;'),
+  ('"', '&quot;'),
+  ("'", '&apos;'),
+  ('\n', '&#xA;')
+]
+
+def pushTop(stack, d, root_node):
+  root = 'objects' if None == root_node else root_node
+  top = {
+    'obj': d,
+    'wrap': False if None == root_node or isinstance(d, list) else True,
+    'root': root,
+    'root_singular': root[:-1] if 's' == root[-1] and None == root_node else root,
+    'xml': '',
+    'children': [],
+    'parent': stack[0] if len(stack) > 0 else None
+  }
+  stack.append(top)
+  return top
+
+def popTop(stack):
+  top = stack.pop()
+
+  end_tag = '>' if 0 < len(top['children']) else '/>'
+
+  if top['wrap'] or isinstance(top['obj'], dict):
+    top['xml'] = '<' + top['root'] + top['xml'] + end_tag
+
+  if 0 < len(top['children']):
+    for child in top['children']:
+      top['xml'] = top['xml'] + child
+
+    if top['wrap'] or isinstance(top['obj'], dict):
+      top['xml'] = top['xml'] + '</' + top['root'] + '>'
+
+  return top;
+
+def isValidList(l):
+  if not isinstance(l, list):
+    return False
+
+  for v in l:
+    if not isinstance(v, dict) or isValidList(v):
+      return False
+
+def listToStr(l):
+  text = ''
+  for v in l:
+    if py2 and type(v) == unicode:
+      text += '&lt;' + v + '&gt;'
+    else:
+      text += '&lt;' + str(v) + '&gt;'
+
+  return text
+
 def toXml(d, root_node=None):
-  wrap          =     False if None == root_node or isinstance(d, list) else True
-  root          = 'objects' if None == root_node else root_node
-  root_singular = root[:-1] if 's' == root[-1] and None == root_node else root
-  xml           = ''
-  children      = []
-
-  charmap = [
-    # Note that `&->&amp;` must be the first item.
-    ('&', '&amp'),
-    ('<', '&lt;'),
-    ('>', '&gt;'),
-    ('"', '&quot;'),
-    ("'", '&apos;'),
-    ('\n', '&#xA;')
-  ]
+  stack = []
+  top = pushTop(stack, d, root_node)
   
-
-  if isinstance(d, dict):
-    for key, value in dict.items(d):
-      if isinstance(value, dict):
-        children.append(toXml(value, key))
-      elif isinstance(value, list):
-        children.append(toXml(value, key))
+  while True:
+    if 'ready' not in top:
+      top['ready'] = True
+      if isinstance(top['obj'], dict):
+        for key, value in dict.items(top['obj']):
+          if isinstance(value, dict):
+            top = pushTop(stack, value, key)
+          elif isinstance(value, list):
+            if isValidList(value):
+              top = pushTop(stack, value, key)
+            else:
+              top['xml'] += ' ' + key + '="' + listToStr(value) + '"'
+          elif value != None:
+            # Check for invalid characters:
+            for char, code in charmap:
+              assert(char not in key)
+              value = str(value).replace(char, code)
+            # Add attributes:
+            top['xml'] = top['xml'] + ' ' + key + '="' + value + '"'
+      elif isinstance(top['obj'], list):
+        for value in top['obj']:
+          top = pushTop(stack, value, top['root_singular'])
       else:
-        for char, code in charmap:
-          assert(char not in key)
-          value = str(value).replace(char, code)
+        raise Exception(
+            "Invalid type `%s` to be converted to XML!" % type(top['obj']))
 
-        xml = xml + ' ' + key + '="' + value + '"'
-  else:
-    for value in d:
-      children.append(toXml(value, root_singular))
-
-  end_tag = '>' if 0 < len(children) else '/>'
-
-  if wrap or isinstance(d, dict):
-    xml = '<' + root + xml + end_tag
-
-  if 0 < len(children):
-    for child in children:
-      xml = xml + child
-
-    if wrap or isinstance(d, dict):
-      xml = xml + '</' + root + '>'
+    if 'ready' in top:
+      if top['parent'] == None:
+        break
+      else:
+        top = popTop(stack)
+        top['parent']['children'].append('\n'+top['xml'])
+        top = stack[-1]
     
-  return xml
+  return popTop(stack)['xml']
 
 if __name__ == '__main__':
   
